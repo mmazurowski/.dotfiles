49145ed921b232b3a5bb56d5dab1de21050955b7
Marcin Mazurowski <hello@ktwstudio.com>
2024-07-08 11:13:41 +0200

    feat: Close NeoTree when opening file

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,3 +1,38 @@
+local function deep_copy(object)
+  if type(object) ~= "table" then
+    return object
+  end
+
+  local result = {}
+  for key, value in pairs(object) do
+    result[key] = deep_copy(value)
+  end
+  return result
+end
+
+local function spread(template, override)
+  if not override then
+    return function(override)
+      spread(template, override)
+    end
+  end
+
+  local mt = getmetatable(template)
+  local result = {}
+  setmetatable(result, mt)
+
+  for key, value in pairs(template) do
+    result[key] = deep_copy(value)
+  end
+
+  -- No longer wrapped up inside a function
+  for key, value in pairs(override) do
+    result[key] = value
+  end
+
+  return result
+end
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -55,7 +90,20 @@ return {
       },
     },
     config = function(_, opts)
-      require("neo-tree").setup(opts)
+      local result = spread(opts, {
+        event_handlers = {
+          {
+            event = "file_open_requested",
+            handler = function()
+              -- auto close
+              -- vimc.cmd("Neotree close")
+              -- OR
+              require("neo-tree.command").execute({ action = "close" })
+            end,
+          },
+        },
+      })
+      require("neo-tree").setup(result)
       vim.api.nvim_create_autocmd("TermClose", {
         pattern = "*lazygit",
         callback = function()
